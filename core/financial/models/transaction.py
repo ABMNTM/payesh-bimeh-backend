8c6907f7cf4b0154cef8d90bb4f1d30b86c6a8a6
mehrabi <mehrabiabol1383@gmail.com>
2026-08-19 10:31:46 +0330

    Feat: payment implemented.

---
+++ core/financial/models/transaction.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+from financial.models.payment import Payment
+
+
+class Transaction(models.Model):
+    authority = models.CharField(
+        max_length=255,
+        unique=True,
+        verbose_name="شناسه نشست پرداخت",
+    )
+
+    reference_id = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name="شناسه مرجع پرداخت",
+    )
+
+    amount = models.PositiveBigIntegerField(
+        verbose_name="مبلغ پرداختی",
+    )
+
+    enrollment = models.ForeignKey(
+        "main.Enrollment",
+        models.CASCADE,
+        related_name="transactions",
+        verbose_name="ثبت نام بیمه",
+    )
+
+    success = models.BooleanField(
+        default=False, verbose_name="موفقیت آمیز؟"
+    )
+
+    paid_at = models.DateTimeField(null=True, blank=True, verbose_name="تاریخ پرداخت")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="زمان ساخت")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="زمان بروزرسانی")
+
+    def __str__(self):
+        return self.authority
+
+    def payment_request(self):
+        response = Payment(self.amount).request()
+        self.authority = response.get("authority")
+
+        self.save()
+        return self.authority
+
+    def payment_verify(self):
+        response = Payment(self.amount).verify()
+        if success:= response.get("success"):
+            self.success = True
+            self.reference_id = response.get("reference_id")
+            self.paid_at = response.get("paid_at")
+            self.save()
+        return success
