56f578ec01c71aac744efbea81db8661e3fc9f73
mehrabi <mehrabiabol1383@gmail.com>
2026-09-12 20:31:47 +0330

    Feat: Transaction model removed.

--- core/financial/admin/__init__.py
+++ core/financial/admin/__init__.py
@@ -1,3 +1,2 @@
 from .cost_rate_rule import CostRateRuleAdmin
 from .exceptional_condition import ExceptionalCondition
-from .transaction import TransactionAdmin
--- core/financial/models/__init__.py
+++ core/financial/models/__init__.py
@@ -1,3 +1,2 @@
-from .transaction import Transaction
 from .cost_rate_rule import CostRateRule
 from .exceptional_condition import ExceptionalCondition
